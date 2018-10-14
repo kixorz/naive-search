a6a426de75899737420edae677767d1bddaff0b1
Adam Konrad <kixorz@gmail.com>
2018-10-14 13:14:30 -0500

    schema adjustment

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,14 @@
-CREATE TABLE IF NOT EXISTS pages (
+DROP TABLE pages;
+
+CREATE TABLE pages (
   page_id INT AUTO_INCREMENT,
   url TEXT NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   PRIMARY KEY (page_id)
 );
+
+ALTER TABLE pages ADD FULLTEXT (url, title, description);
 
 #LOAD DATA LOCAL INFILE 'test.csv' INTO TABLE pages (url, title, description)
 #LOAD DATA LOCAL INFILE 'search' INTO TABLE pages (url, title, description) FIELDS TERMINATED BY ',' ENCLOSED BY '"'
