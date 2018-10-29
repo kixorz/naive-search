3a64dc52348ef609568b6c4b7a54a91ddc8babb3
Adam Konrad <kixorz@gmail.com>
2018-10-29 01:09:46 -0500

    disabled db connection

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var search = require('../models/search');
+//var search = require('../models/search');
 
 router.get('/', function (req, res, next) {
 	res.render('index', {title: 'Search'});
