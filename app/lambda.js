45dac53f3e4eaa038975b0968dd984df54d93643
Adam Konrad <kixorz@gmail.com>
2018-10-29 23:32:04 -0500

    another binary test

--- app/lambda.js
+++ app/lambda.js
@@ -1,7 +1,27 @@
 var serverless = require('aws-serverless-express');
 var app = require('./app');
 
-var server = serverless.createServer(app);
+var binaryMimeTypes = [
+	'application/javascript',
+	'application/json',
+	'application/octet-stream',
+	'application/xml',
+	'font/eot',
+	'font/opentype',
+	'font/otf',
+	'image/jpeg',
+	'image/png',
+	'image/svg+xml',
+	'text/comma-separated-values',
+	'text/css',
+	'text/html',
+	'text/javascript',
+	'text/plain',
+	'text/text',
+	'text/xml'
+];
+
+var server = serverless.createServer(app, null, binaryMimeTypes);
 
 exports.handler = function (event, context) {
 	return serverless.proxy(server, event, context);
