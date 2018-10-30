aa28f2bd33ed28fb00830dfafdffd8fe49b0dfc5
Adam Konrad <kixorz@gmail.com>
2018-10-30 00:55:26 -0500

    root event source

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var middleware = require('aws-serverless-express/middleware');
 
 var app = express();
@@ -10,10 +11,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(middleware.eventContext());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(middleware.eventContext());
 
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
