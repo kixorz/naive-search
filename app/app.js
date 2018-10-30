9a141c3a26514613add526a29d0e0f6941285aa0
Adam Konrad <kixorz@gmail.com>
2018-10-30 01:18:37 -0500

    removed bodyparser

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var middleware = require('aws-serverless-express/middleware');
 
@@ -13,7 +12,6 @@ app.set('view engine', 'jade');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(middleware.eventContext());
