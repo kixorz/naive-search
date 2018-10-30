0f9992482f0baec8968d6182163bc59944f20735
Adam Konrad <kixorz@gmail.com>
2018-10-30 00:03:36 -0500

    binary media types don't work

--- app/lambda.js
+++ app/lambda.js
@@ -1,27 +1,7 @@
 var serverless = require('aws-serverless-express');
 var app = require('./app');
 
-var binaryMimeTypes = [
-	'application/javascript',
-	'application/json',
-	'application/octet-stream',
-	'application/xml',
-	'font/eot',
-	'font/opentype',
-	'font/otf',
-	'image/jpeg',
-	'image/png',
-	'image/svg+xml',
-	'text/comma-separated-values',
-	'text/css',
-	'text/html',
-	'text/javascript',
-	'text/plain',
-	'text/text',
-	'text/xml'
-];
-
-var server = serverless.createServer(app, null, binaryMimeTypes);
+var server = serverless.createServer(app);
 
 exports.handler = function (event, context) {
 	return serverless.proxy(server, event, context);
