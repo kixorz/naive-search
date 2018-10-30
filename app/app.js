7ad9e91d2591a264b90e12257ff290668d7a17b1
Adam Konrad <kixorz@gmail.com>
2018-10-30 00:23:48 -0500

    testing binary types

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var middleware = require('aws-serverless-express/middleware');
 
 var app = express();
@@ -11,12 +10,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(middleware.eventContext());
+app.use(express.static(path.join(__dirname, 'public')));
 
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
