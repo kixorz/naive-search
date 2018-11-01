83194bfef9aa5f88f95edc7a4a0d3bd95e7c283c
Adam Konrad <kixorz@gmail.com>
2018-10-31 23:54:03 -0500

    enabled fulltext indices

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,19 @@ CREATE TABLE IF NOT EXISTS pages (
   url TEXT NOT NULL,
   title TEXT NOT NULL,
   description TEXT NOT NULL,
+  FULLTEXT ux (url),
+  FULLTEXT tx (title),
+  FULLTEXT dx (description),
   PRIMARY KEY (page_id)
 );
 
-#ALTER TABLE pages ADD FULLTEXT (url, title, description);
+#CREATE FULLTEXT INDEX ON pages (url, title, description);
+
+#ALTER TABLE pages ADD FULLTEXT (url);
+#ALTER TABLE pages ADD FULLTEXT (title);
+#ALTER TABLE pages ADD FULLTEXT (description);
+
+#SHOW INDEX FROM pages;
 
 #LOAD DATA LOCAL INFILE 'test.csv' INTO TABLE pages (url, title, description)
 #LOAD DATA LOCAL INFILE 'search' INTO TABLE pages (url, title, description) FIELDS TERMINATED BY ',' ENCLOSED BY '"'
