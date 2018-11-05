9e4da8ff987f79653102284f03ea2f837a4d1712
Adam Konrad <kixorz@gmail.com>
2018-11-04 21:42:55 -0600

    adjusted offset normalization

--- app/routes/search.js
+++ app/routes/search.js
@@ -5,7 +5,7 @@ var search = require('../models/search');
 router.get('/', function (req, res, next) {
 	var query = req.query['query'];
 	var limit = 10;
-	var offset = Math.max(parseInt(req.query['offset'], 10), 0);
+	var offset = Math.max(parseInt(req.query['offset'], 10) || 0, 0);
 	search.query(query, limit, offset, function (error, results) {
 		if (error) {
 			res.render('error', {
